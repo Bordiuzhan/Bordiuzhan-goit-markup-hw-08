587972604025d7098c52ff714c751d80ed4e47d6
Bordiuzhan <bordyujan@gmail.com>
2022-07-11 00:23:15 +0200

    hw 8 last

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,7 @@
   const refs = {
     openMenuBtn: document.querySelector(".menu-open-btn"),
     closeMenuBtn: document.querySelector(".menu-close-btn"),
-    menu: document.querySelector(".menu-mob"),
+    menu: document.querySelector(".mob-menu"),
   };
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
